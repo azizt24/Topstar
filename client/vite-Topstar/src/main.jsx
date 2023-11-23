0b241cb7bd515836b8b81f6803a03ab3a776cb95
azizt24 <aziztouma030@gmail.com>
2023-11-23 14:41:36 +0200

    new index

--- client/vite-Topstar/src/main.jsx
+++ client/vite-Topstar/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/index.jsx
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>,
-)
+  </Router>,
+  document.getElementById('root')
+);
