3e381216b776075192435aab8a0ca1f06c9eaf8e
azizt24 <aziztouma030@gmail.com>
2023-11-25 14:43:03 +0200

    new

--- client/vite-Topstar/src/pages/products/Products.jsx
+++ client/vite-Topstar/src/pages/products/Products.jsx
@@ -1,13 +1,33 @@
  
-import React from 'react';
+ 
 
-function Products() {
+import React, { useState, useEffect } from 'react';
+
+const PerfumeList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+ 
+    fetch('http://localhost:3000/products')
+      .then(response => response.json() ).then(data => console.log(data))
+      .then(data => setProducts(data.data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
   return (
     <div>
-      <h1>Our Products</h1>
-       
+      <h1>Perfume Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.Image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>{product.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default Products;
+export default PerfumeList;
